903bcc1ce9197c8cdc33bae3b75af1dd8b30cca4
elaine <elainemuhombe.codingal@gmail.com>
2025-04-18 09:31:38 +0300

    refactor: modify pdatabase file path

--- analyze_tables.py
+++ analyze_tables.py
@@ -1,5 +1,5 @@
 # Import file from your system
-file_path = "c:/Users/ELAINE_DEV/Documents/Lesson-74-SQLLite-Python-activity-acp/basketball.sqlite"
+file_path = "c:/Users/ELAINE_DEV/Documents/Lesson-74-SQLLite-Python-activity-acp/database.sqlite"
 print("Selected file:", file_path)
 
 
